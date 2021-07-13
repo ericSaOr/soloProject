45cb8a9be91f91dbbc9b8aada62b9f940cc2d022
Eric Sanchez <ericfsod@gmail.com>
2021-07-13 18:28:53 -0400

    changed the number of character parameter in the fetch request to 50

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ console.log('Hi!')
 
 function fetchLOTRData (data){
     console.log("Function Invoked!")
-    fetch('https://the-one-api.dev/v2/character?limit=10', {
+    fetch('https://the-one-api.dev/v2/character?limit=50', {
         //in documentation it says to enter character(s)?limit=someNumber. WRONG. 
         method: "GET", headers: {
             Authorization: "Bearer h32ntxCf7_hsaBM8g3G3"
@@ -35,15 +35,33 @@ function renderLOTRCard (ringObject){
     
     //console.log(ringObject.docs[0])
 
-    console.log(ringObject.docs[0].name)
+    console.log(ringObject.docs)
  
     
     // 
-    for(i = 0; i < ringObject.docs.length; i++) {
+    for(let i = 0; i < ringObject.docs.length; i++) {
         console.log("hi")
         let nameElement = document.createElement('h2')
-        nameElement.textContent=ringObject.docs.indexOf(i).name;
+        nameElement.textContent = ringObject.docs[i].name;
         document.getElementById('cardContainer').appendChild(nameElement)
+
+        let raceElement = document.createElement('p')
+        raceElement.textContent = ringObject.docs[i].race;
+        document.getElementById('cardContainer').appendChild(raceElement) 
+
+        let sexElement = document.createElement('p')
+        sexElement.textContent = ringObject.docs[i].gender
+        document.getElementById('cardContainer').appendChild(sexElement)
+
+        let birthElement = document.createElement('p')
+        birthElement.textContent = ringObject.docs[i].birth
+        document.getElementById('cardContainer').appendChild(birthElement)
+
+        let wikiURL = document.createElement('a')
+        wikiURL.textContent = ringObject.docs[i].wikiUrl
+        document.getElementById('cardContainer').appendChild(wikiURL)
+
+
     };
 
     
