13d16b65371bd94a714ee930cce2c0dad7fab9e0
Eric Sanchez <ericfsod@gmail.com>
2021-07-14 14:08:17 -0400

    Added event listener to change color of wikia links to purple, added a button, button spits out name and birthplace

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ console.log('Hi!')
 
 function fetchLOTRData (data){
     console.log("Function Invoked!")
-    fetch('https://the-one-api.dev/v2/character?limit=50', {
+    fetch('https://the-one-api.dev/v2/character?limit=10', {
         //in documentation it says to enter character(s)?limit=someNumber. WRONG. 
         method: "GET", headers: {
             Authorization: "Bearer h32ntxCf7_hsaBM8g3G3"
@@ -20,13 +20,9 @@ function fetchLOTRData (data){
     .then(data => {
         renderLOTRCard(data)
     
-    
-    
     })
     
-    
-    
-    
+     
 }
 
 
@@ -57,19 +53,56 @@ function renderLOTRCard (ringObject){
         birthElement.textContent = ringObject.docs[i].birth
         document.getElementById('cardContainer').appendChild(birthElement)
 
-        let wikiURL = document.createElement('a')
-        wikiURL.textContent = ringObject.docs[i].wikiUrl
-        document.getElementById('cardContainer').appendChild(wikiURL)
-
-
-    };
-
+        let wikiURLEl = document.createElement('a')
+        wikiURLEl.textContent = ringObject.docs[i].wikiUrl;
+        wikiURLEl.href = ringObject.docs[i].wikiUrl;
+        
+        
+        
+        document.getElementById('cardContainer').appendChild(wikiURLEl)
     
     
 
-    //creating 
+        wikiURLEl.addEventListener('click', function(){
+           wikiURLEl.style.color = "purple";
+           })
+            
+           
+            
+        }
+
+        let buttonElement = document.createElement('button');
+        buttonElement.textContent = ("Name/Birthplace")
+        buttonElement.addEventListener('click', function(e){
+            buttonClick(e, ringObject);
+
+        })
+        document.getElementById('buttonContainer').appendChild(buttonElement);
+
+
+
+
+}
+function buttonClick(event, allLOTRdata) {
+
+    for (let i = 0; i < allLOTRdata.docs.length; i++){
+
+    let ringNameElement = document.createElement('p')
+    ringNameElement.textContent = allLOTRdata.docs[i].name;
+    document.getElementById('buttonContainer').appendChild(ringNameElement);
+    
+    let ringBirthElement = document.createElement('p');
+    ringBirthElement.textContent = allLOTRdata.docs[i].birth
+
+    document.getElementById('buttonContainer').appendChild(ringBirthElement)
+
+    
 }
 
 
+}
+
+//allLOTRdata.docs[i].birth;
 //h32ntxCf7_hsaBM8g3G3
 //Authorization: Bearer your-api-key-123
+//<div id = "buttonContainer">
